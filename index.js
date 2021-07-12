536dcbd0d73fee18da09b419ce428816905e37ba
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-07-12 16:23:24 +0200

    wip(): list secrets

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function getOcto(token) {
+function getOcto() {
+  const token = core.getInput('token') ?? process.env.GITHUB_TOKEN;
+  return github.getOctokit(token)
 }
 
-(() => {
+(async () => {
   // `who-to-greet` input defined in action metadata file
   const keystone_slot = core.getInput('keystone_slot');
+  const repo = github.context.repo.repo.name;
+  const owner = github.context.repo.owner.name;
+  const octoOptions = {
+    repo,
+    owner
+  }
+  const octo = getOcto();
+
+  const secrets = await octo.rest.actions.listRepoSecrets(octoOptions);
+  console.log("LS -> index.js:21 -> secrets: ", secrets)
+
   let files, secrets;
 
   try {
