0e22af3d8aaec7f62fa098b9ca922e3cca8d4f54
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-07-13 10:07:38 +0200

    feat(): cleaner output

--- index.js
+++ index.js
@@ -1,59 +1,37 @@
 const fs = require('fs');
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 
-function getOcto() {
-  const token = core.getInput('token') || process.env.GITHUB_TOKEN;
-  return github.getOctokit(token)
-}
-
-(async () => {
-  const payload = JSON.stringify(github, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log("The action environment:", JSON.stringify(process.env, undefined, 2))
-
-  // `who-to-greet` input defined in action metadata file
-  const keystone_slot = core.getInput('keystone_slot');
-
-  const repo = github.context.payload.repository.name;
-  const owner = github.context.payload.repository.owner.name;
-
-  const octoOptions = {
-    repo,
-    owner
-  }
-  console.log("LS -> index.js:18 -> octoOptions: ", JSON.stringify(octoOptions, undefined, 2))
-  const octo = getOcto();
-
-  let repoSecrets = [];
-
-  try {
-    repoSecrets = await octo.rest.actions.listRepoSecrets(octoOptions);
-    console.log("LS -> index.js:21 -> secrets: ", JSON.stringify(repoSecrets, undefined, 3))
-  } catch (err) {
-    core.setFailed(err.message)
-    return
-  }
-
+(() => {
+  const keystone_slot = core.getInput(`keystone_slot`);
   let files, secrets;
 
   try {
-    const message = JSON.parse(keystone_slot)
-    files = message.files
-    secrets = message.secrets
+    const message = JSON.parse(keystone_slot);
+    files = message.files;
+    secrets = message.secrets;
   } catch (err) {
     core.setFailed(err.message);
-    return
+    return;
   }
 
   secrets.forEach(({ label, value }) => {
     core.setSecret(value);
     core.exportVariable(label, value);
+    core.info(`Loaded ${label}`);
   })
 
   files.forEach(({ path, content }) => {
-    fs.writeFileSync(path, content, { mode: 0o644 })
-  })
+    const dirname = path.dirname(path);
 
+    try {
+      fs.mkdirSync(dirname, { recursive: true });
+      fs.writeFileSync(path, content, { mode: 0o644 });
+
+      core.info(`Wrote ${path}`);
+    } catch (err) {
+      core.error(err);
+    }
+  });
 })()
 
