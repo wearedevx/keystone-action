494bc1921e89eba701cf60d50a93949de60a9513
Abigael <abigael@wearedevx.com>
2021-07-22 10:47:09 +0200

    fix(slots) get message from slots

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const core = require('@actions/core');
+const fs = require("fs");
+const path = require("path");
+const core = require("@actions/core");
 // const github = require('@actions/github');
 
 (() => {
-  const keystone_slot = core.getInput(`keystone_slot`);
+  const keystone_slot_1 = core.getInput(`keystone_slot_1`);
+  const keystone_slot_2 = core.getInput(`keystone_slot_2`);
+  const keystone_slot_3 = core.getInput(`keystone_slot_3`);
+  const keystone_slot_4 = core.getInput(`keystone_slot_4`);
+  const keystone_slot_5 = core.getInput(`keystone_slot_5`);
+  keystone_slot =
+    keystone_slot_1 +
+    keystone_slot_2 +
+    keystone_slot_3 +
+    keystone_slot_4 +
+    keystone_slot_5;
+
   let files, secrets;
 
   try {
@@ -20,23 +31,22 @@ const core = require('@actions/core');
     core.setSecret(value);
     core.exportVariable(label, value);
     core.info(`Loaded ${label}`);
-  })
+  });
 
   files.forEach((file) => {
     const dirname = path.dirname(file.path);
-    const buffer = Buffer.from(file.content, 'base64')
+    const buffer = Buffer.from(file.content, "base64");
 
     try {
       fs.mkdirSync(dirname, { recursive: true });
-      fs.writeFileSync(file.path, buffer, { mode: 0o644, encoding: 'utf8' });
+      fs.writeFileSync(file.path, buffer, { mode: 0o644, encoding: "utf8" });
 
       // also set the content of the file a secret
-      core.setSecret(buffer.toString('utf8'));
+      core.setSecret(buffer.toString("utf8"));
 
       core.info(`Wrote ${file.path}`);
     } catch (err) {
       core.error(err);
     }
   });
-})()
-
+})();
