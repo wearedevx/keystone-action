951bbe6ce8a987c8a52575fe9a9fe64401581b57
Abigael <abigael@wearedevx.com>
2021-07-28 15:33:49 +0200

    debug

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const core = require("@actions/core");
     keystone_slot_3 +
     keystone_slot_4 +
     keystone_slot_5;
+  console.log(keystone_slot.replace('"', "'").replace("a","e").replace("e","r"));
 
   let files, secrets;
 
