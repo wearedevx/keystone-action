ee489bb0626e367882d2fcc241026f8d5197f145
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-08-03 16:41:00 +0200

    feat(multiline): handle escaped chars

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ const core = require("@actions/core");
 
   secrets.forEach(({ label, value }) => {
     core.setSecret(value);
-    core.exportVariable(label, value);
+    const unescapedValue = unescape(value);
+
+    core.exportVariable(label, unescapedValue);
     core.info(`Loaded ${label}`);
   });
 
@@ -50,3 +52,34 @@ const core = require("@actions/core");
     }
   });
 })();
+
+const R = /\\([^$])/gm
+
+function unescape(value) {
+  const origLength = value.length
+  let escaped = value
+  let escapedLength = escaped.length
+
+  const matches = value.matchAll(R)
+
+  for (let m of matches) {
+    let [whole, replacement] = m
+
+    if (replacement == "n") {
+       replacement = "\n"
+    } else if (value == "r") {
+      replacement = "\r"
+    }
+
+    const diff = origLength - escapedLength;
+    const i = m.index - diff
+
+    escaped = escaped.substring(0, i)
+      + replacement
+      + escaped.substring(i + whole.length)
+
+    escapedLength = escaped.length
+  }
+
+  return escaped
+}
