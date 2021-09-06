b80e94962f4d78915b0aa7769a6d2566d2296bd8
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-09-06 11:32:49 +0200

    fix() module keystone-file not found

--- keystone-file.js
+++ keystone-file.js
@@ -1,29 +1,31 @@
 /**
-  * @typedef  {Object}              KeystoneFile
-  * @property {string}              project_id
-  * @property {string}              name
-  * @property {Array<EnvKey>}       env
-  * @property {Array<FileKey>}      files
-  * @property {KeystoneFileOptions} options
-  * @property {Array<CiService>}    ci_services
-  */
+ * @typedef  {Object}              KeystoneFile
+ * @property {string}              project_id
+ * @property {string}              name
+ * @property {Array<EnvKey>}       env
+ * @property {Array<FileKey>}      files
+ * @property {KeystoneFileOptions} options
+ * @property {Array<CiService>}    ci_services
+ */
 
 /**
-  * @typedef  {Object} EnvKey
-  * @property {string} key
-  * @property {bool}   strict
-  */
+ * @typedef  {Object} EnvKey
+ * @property {string} key
+ * @property {bool}   strict
+ */
 
 /**
-  * @typedef  {Object} FileKey
-  * @property {string} path
-  * @property {bool}   strict
-  * @property {bool}   fromcache
-  */
+ * @typedef  {Object} FileKey
+ * @property {string} path
+ * @property {bool}   strict
+ * @property {bool}   fromcache
+ */
 
 /**
-  * @typedef {Object} CiService
-  * @property {string} name
-  * @property {string} type 
-  * @property {Object} options
-  */
+ * @typedef {Object} CiService
+ * @property {string} name
+ * @property {string} type
+ * @property {Object} options
+ */
+
+module.exports = {};
