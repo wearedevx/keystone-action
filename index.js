524520348d4d1cdf08f3c842dbb736e3792fad05
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-11-12 12:19:49 +0100

    feat: only need to write the files now

--- index.js
+++ index.js
@@ -2,200 +2,85 @@ const fs = require("fs");
 const path = require("path");
 const core = require("@actions/core");
 
-const yaml = require("yaml");
 const gitignore = require("./gitignore");
-
-/**
- * @typedef {import("./keystone-file.js").KeystoneFile} KeystoneFile
- * @typedef {import("./keystone-file.js").EnvKey} EnvKey
- * @typedef {import("./keystone-file.js").FileKey} FileKey
- */
-
-/**
- * @typedef {Object} Secret
- * @property {string} label
- * @property {string} value
- */
 
 /**
  * @typedef {Object} File
  * @property {string} path
- * @property {string} content
+ * @property {Buffer} content
  */
 
 /**
- * @returns {KeystoneFile}
+ * @returns {{ files: File[] }}
  */
-function getKeystoneFile() {
-  const contents = fs.readFileSync(
-    path.join(process.cwd(), "keystone.yaml"),
-    "utf-8"
-  );
+function decodeInputs() {
+  const filesRaw = core.getInput("files");
 
-  const result = yaml.parse(contents);
+  const files = filesRaw.split(",").map((v) => {
+    const [path, base64content] = v.split("#");
+    const content = Buffer.from(base64content, "base64");
 
-  return {
-    env: [],
-    files: [],
-    ...result,
-  };
+    return { path, content };
+  });
+
+  return { files };
 }
 
 /**
- * @param {EnvKey} secretDefininition
- * @param {Secret[]} secrets
- *
- * @returns {bool}
+ * Promisified version of fs.mkdir()
+ * @param {string} path
+ * @param {fs.MakeDirectoryOptions} options
+ * @returns {Promise<*,Error>}
  */
-function isSecretMissing(secretDefininition, secrets) {
-  let result = false;
-
-  if (secretDefininition.strict) {
-    const s = secrets.find(({ label }) => label === secretDefininition.key);
-
-    if (s === null || s === undefined) {
-      result = true;
-    } else if (!s.value) {
-      result = true;
-    }
-  }
-
-  return result;
+function mkdir(path, options) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path, options, (err, ...result) => {
+      if (err) reject(err);
+      else resolve(...result);
+    });
+  });
 }
 
 /**
- * @param {FileKey} fileDefinition
- * @param {File[]} files
- *
- * @returns {bool}
+ * Promisified version of fs.writeFile()
+ * @param {string} path
+ * @param {string|Buffer} content
+ * @param {fs.WriteFileOptions} options
+ * @returns {Promise<*,Error>}
  */
-function isFileMissing(fileDefinition, files) {
-  let result = false;
-
-  if (fileDefinition.strict) {
-    const s = files.find(({ path }) => path == fileDefinition.path);
-
-    if (s === null || s === undefined) {
-      result = true;
-    } else if (s.content === "") {
-      result = true;
-    }
-  }
-
-  return result;
+function writeFile(path, content, options) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, content, options, (err, ...result) => {
+      if (err) reject(err);
+      else resolve(...result);
+    });
+  });
 }
 
 /**
- * @param {EnvKey[]} secretDefinitions
- * @param {Secret[]} secrets
- *
- * @returns {string[]}
+ * Writes the file, creating the necessary directories
+ * @param {File} file
+ * @return {Promise<undefined, Error>}
  */
-function missingSecrets(secretDefinitions, secrets) {
-  return secretDefinitions
-    .filter((sd) => isSecretMissing(sd, secrets))
-    .map((sd) => sd.key);
-}
+async function write(file) {
+  const dirname = path.dirname(file.path);
 
-/**
- * @param {FileKey[]} fileDefinitions
- * @param {File[]} files
- *
- * @returns {string[]}
- */
-function missingFiles(fileDefinitions, files) {
-  return fileDefinitions
-    .filter((fd) => isFileMissing(fd, files))
-    .map((fd) => fd.path);
-}
-
-/**
- * @returns {files: File[], secrets: Secret[], error: Error | null}
- */
-function decodeKeystoneSlots() {
-  const keystone_slot_1 = core.getInput(`keystone_slot_1`);
-  const keystone_slot_2 = core.getInput(`keystone_slot_2`);
-  const keystone_slot_3 = core.getInput(`keystone_slot_3`);
-  const keystone_slot_4 = core.getInput(`keystone_slot_4`);
-  const keystone_slot_5 = core.getInput(`keystone_slot_5`);
-
-  const keystone_slot =
-    keystone_slot_1 +
-    keystone_slot_2 +
-    keystone_slot_3 +
-    keystone_slot_4 +
-    keystone_slot_5;
-
-  let files, secrets;
-
-  try {
-    const message = JSON.parse(keystone_slot);
-    files = message.files;
-    secrets = message.secrets;
-  } catch (err) {
-    return { error: err };
-  }
-
-  return { files, secrets, error: null };
+  await mkdir(dirname, { recursive: true });
+  await writeFile(file.path, file.content, { mode: 0o644, encoding: "binary" });
 }
 
 // Main
 (async () => {
   await gitignore.init();
 
-  let { files, secrets, error } = decodeKeystoneSlots();
+  const { files } = decodeInputs();
 
-  if (error != null) {
-    core.setFailed(error);
-    return;
-  }
-
-  const ksFile = getKeystoneFile();
-  let missing;
-  missing = missingSecrets(ksFile.env, secrets);
-
-  if (missing && missing.length) {
-    const errorMessage = `Some required secrets are missing: ${missing.join(
-      ", "
-    )}`;
-
-    core.setFailed(new Error(errorMessage));
-    return;
-  }
-
-  missing = missingFiles(ksFile.files, files);
-  if (missing && missing.length) {
-    const errorMessage = `Some required files are missing: ${missing.join(
-      ", "
-    )}`;
-
-    core.setFailed(new Error(errorMessage));
-    return;
-  }
-
-  ksFile.env.forEach(({ key }) => {
-    const found = secrets.find((s) => s.label === key);
-    if (!found) return;
-
-    const { label, value } = found;
-
-    core.setSecret(value);
-    core.exportVariable(label, value);
-    core.info(`Loaded ${label}`);
-  });
-
-  for (let fd of ksFile.files) {
-    const file = files.find((f) => f.path === fd.path);
-
-    const dirname = path.dirname(file.path);
-    const buffer = Buffer.from(file.content, "base64");
-
+  for (let file of files) {
     try {
-      fs.mkdirSync(dirname, { recursive: true });
-      fs.writeFileSync(file.path, buffer, { mode: 0o644, encoding: "utf8" });
+      await write(file);
 
       // also set the content of the file a secret
-      core.setSecret(buffer.toString("utf8"));
+      core.setSecret(file.content.toString("utf8"));
 
       // add to gitignore file if not in there
       await gitignore.add(file.path);
